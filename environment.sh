d220eaf26ebeea4a2efa2ac4eb678a643a481626
Wolf Chen <andy11259@yahoo.com.tw>
2019-07-23 22:44:40 +0800

    add set_ros_master

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
 source /opt/ros/melodic/setup.bash
 source $HOME/dehui_ws/catkin_ws/devel/setup.bash
 export ROS_IP=$HOSTNAME.local
-export ROS_MASTER_RUI=http://zedm01.local:11311/
+source set_ros_master.sh zedm01

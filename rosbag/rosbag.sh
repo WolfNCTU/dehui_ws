c253f873c0efbf891b00c6472c1c12501426e7f7
AndySer37 <andy19970307@gmail.com>
2019-07-28 11:57:45 +0800

    change namespace

--- rosbag/rosbag.sh
+++
@@ -1,1 +0,0 @@
-rosbag record --split --duration=3m -e /zedm01/zed/point_cloud/cloud_registered /zedm01/zed/rgb/camera_info /zedm01/zed/rgb/image_rect_color /zedm01/zed/depth/camera_info /zedm01/zed/depth/depth_registered
